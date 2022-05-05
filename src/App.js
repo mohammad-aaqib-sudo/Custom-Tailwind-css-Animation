0258828ab95b4dd92c94a379c47127d2d8e3cee1
mohammad-aaqib-sudo <aqib5325@gmail.com>
2022-05-05 11:17:10 +0530

    added text

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App ">
-      <div className="bg-green-100 w-full min-h-screen items-center justify-center flex">
+      <div className="bg-green-100 w-full min-h-screen items-center justify-center flex flex-col">
+        <p className="text-xl text-green-600 py-2">Hi Everyone !</p>
         <p className=" text-9xl animate-wiggle">&#x270B;</p>
       </div>
     </div>
